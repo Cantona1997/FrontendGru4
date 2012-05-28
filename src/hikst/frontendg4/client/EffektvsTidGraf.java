c97a78c11b6939738b9e2a63897549743b2cf099
Thomas Johansen <thomasjohansen89@gmail.com>
2012-05-28 14:07:58 +0200

    Only double values in graph

--- src/hikst/frontendg4/client/EffektvsTidGraf.java
+++ src/hikst/frontendg4/client/EffektvsTidGraf.java
@@ -6,41 +6,43 @@ import com.googlecode.gchart.client.GChart;
 
 public class EffektvsTidGraf extends GChart {
 	
-	static class DateStampedValue {
+	static class GraphValues {
 	     Date date;
 	     double value;
-	     public DateStampedValue(String dateTimeString, double value) {
-	     this.date = new Date(dateTimeString);
+	     double tidsenhet;
+	     public GraphValues(double tidsenhet, double value) {
+	   //  this.date = new Date(dateTimeString);
 	     this.value = value;
+	     this.tidsenhet = tidsenhet;
 	     }
 	   }
 	
-	 DateStampedValue[] dateSequence = {
+	 GraphValues[] dateSequence = {
 			   
-			    new DateStampedValue("5/23/2012 00:00", 80.0),
-			    new DateStampedValue("5/23/2012 01:00", 80.0),
-			    new DateStampedValue("5/23/2012 02:00", 40.0),
-			    new DateStampedValue("5/23/2012 03:00", 40.0),
-			    new DateStampedValue("5/23/2012 04:00", 80.0),
-			    new DateStampedValue("5/23/2012 05:00", 80.0),
-			    new DateStampedValue("5/23/2012 06:00", 80.0),
-			    new DateStampedValue("5/23/2012 07:00", 80.0),
-			    new DateStampedValue("5/23/2012 08:00", 120.0),
-			    new DateStampedValue("5/23/2012 09:00", 120.0),
-			    new DateStampedValue("5/23/2012 10:00", 120.0),
-			    new DateStampedValue("5/23/2012 11:00", 160.0),
-			    new DateStampedValue("5/23/2012 12:00", 160.0),
-			    new DateStampedValue("5/23/2012 13:00", 200.0),
-			    new DateStampedValue("5/23/2012 14:00", 280.0),
-			    new DateStampedValue("5/23/2012 15:00", 320.0),
-			    new DateStampedValue("5/23/2012 16:00", 320.0),
-			    new DateStampedValue("5/23/2012 17:00", 320.0),
-			    new DateStampedValue("5/23/2012 18:00", 320.0),
-			    new DateStampedValue("5/23/2012 19:00", 200.0),
-			    new DateStampedValue("5/23/2012 20:00", 200.0),
-			    new DateStampedValue("5/23/2012 21:00", 160.0),
-			    new DateStampedValue("5/23/2012 22:00", 160.0),
-			    new DateStampedValue("5/23/2012 23:00", 120.0),
+			    new GraphValues(1, 80.0),
+			    new GraphValues(2, 80.0),
+			    new GraphValues(3, 40.0),
+			    new GraphValues(4, 40.0),
+			    new GraphValues(5, 80.0),
+			    new GraphValues(6, 80.0),
+			    new GraphValues(7, 80.0),
+			    new GraphValues(8, 80.0),
+			    new GraphValues(9, 120.0),
+			    new GraphValues(10, 120.0),
+			    new GraphValues(11, 120.0),
+			    new GraphValues(12, 160.0),
+			    new GraphValues(13, 160.0),
+			    new GraphValues(14, 200.0),
+			    new GraphValues(15, 280.0),
+			    new GraphValues(16, 320.0),
+			    new GraphValues(17, 320.0),
+			    new GraphValues(18, 320.0),
+			    new GraphValues(19, 320.0),
+			    new GraphValues(20, 200.0),
+			    new GraphValues(21, 200.0),
+			    new GraphValues(22, 160.0),
+			    new GraphValues(23, 160.0),
+			    new GraphValues(24, 120.0),
 			   };
 	 
 	 EffektvsTidGraf() {
@@ -52,7 +54,7 @@ public class EffektvsTidGraf extends GChart {
 	     getXAxis().setHasGridlines(true);
 	     getXAxis().setTickCount(10);
 	     // Except for "=(Date)", a standard GWT DateTimeFormat string
-	     getXAxis().setTickLabelFormat("=(Date)dd/h:mm a");
+	    // getXAxis().setTickLabelFormat("=(Date)dd/h:mm a");
 	          
 	     getYAxis().setAxisLabel("<small><b><i>WATT</i></b></small>");
 	     getYAxis().setHasGridlines(true);
@@ -68,11 +70,11 @@ public class EffektvsTidGraf extends GChart {
 	     getCurve().getSymbol().setFillThickness(3);
 
 	     for (int i = 0; i < dateSequence.length; i++)
-	       // Note that getTime() returns milliseconds since
-	       // 1/1/70--required whenever "date cast" tick label
-	       // formats (those beginning with "=(Date)") are used.
-	       getCurve().addPoint(dateSequence[i].date.getTime(),
-	                           dateSequence[i].value);
+	    	// Note that getTime() returns milliseconds since
+	    	// 1/1/70--required whenever "date cast" tick label
+	    	// formats (those beginning with "=(Date)") are used.
+	    	getCurve().addPoint(dateSequence[i].tidsenhet,
+	    	dateSequence[i].value);
 	   }
 }
 
